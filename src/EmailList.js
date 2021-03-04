d187de4d350e1b9087a436b3377522b71558e3fb
JoelAngels <joelbaraka4325@gmail.com>
2021-03-04 19:05:14 +0300

    building the section area

--- src/EmailList.js
+++ src/EmailList.js
@@ -1,8 +1,58 @@
+import { Checkbox, IconButton } from "@material-ui/core";
 import React from "react";
 import "./EmailList.css";
-
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import RedIcon from "@material-ui/icons/Redo";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import SettingsIcon from "@material-ui/icons/Settings";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import KeyboardHideIcon from "@material-ui/icons/KeyboardHide";
+import Section from "./Section";
+import InboxIcon from "@material-ui/icons/Inbox";
+import PeopleIcon from "@material-ui/icons/People";
+import LocalOfferIcon from "@material-ui/icons/LocalOffer";
 function EmailList() {
-  return <div className="emailList"></div>;
+  return (
+    <div className="emailList">
+      <div className="emailList__settings">
+        <div className="emailList__settingsLeft">
+          <Checkbox />
+          <IconButton>
+            <ArrowDropDownIcon />
+          </IconButton>
+          <IconButton>
+            <RedIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+
+        <div className="emailList__settingsRight">
+          <IconButton>
+            <ChevronLeftIcon />
+          </IconButton>
+          <IconButton>
+            <ChevronRightIcon />
+          </IconButton>
+          <IconButton>
+            <KeyboardHideIcon />
+          </IconButton>
+          <IconButton>
+            <SettingsIcon />
+          </IconButton>
+        </div>
+      </div>
+
+      {/* This consists of the promotions, socials and Primary*/}
+      <div className="emailList__sections">
+        <Section Icon={InboxIcon} title="primary" color="red" selected />
+        <Section Icon={PeopleIcon} title="social" color="#1A73EB" />
+        <Section Icon={LocalOfferIcon} title="Promotions" color="green" />
+      </div>
+    </div>
+  );
 }
 
 export default EmailList;
---
+++ src/Section.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Section.css";
+
+function Section({ Icon, title, color, selected }) {
+  return <div className="section"></div>;
+}
+
+export default Section;
