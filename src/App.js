751479335d8db09e25b4806f83b3359f0695d920
JoelAngels <joelbaraka4325@gmail.com>
2021-03-04 13:26:51 +0300

    targetted the header, included the gmail picture, menu icon and styled it with flexbox

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="app">
-      <header />
+      <Header />
       <h1>Lets Build Gmail</h1>
     </div>
   );
